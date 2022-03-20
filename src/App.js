1a407b2094a7c265862c09937f2f531bd4ce387c
Vamshi <vamshikrishna424@gmail.com>
2022-03-20 17:11:45 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          In my 4th practice this is 1st commit.
+        </h3>
+        
+        
       </header>
     </div>
   );
